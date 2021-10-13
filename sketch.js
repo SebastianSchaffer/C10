33a865b1ed32a11f97e49c818f6ba6ea3e1311f8
SebastianSchaffer <92061051+SebastianSchaffer@users.noreply.github.com>
2021-10-13 14:17:49 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var weights=[67,73,86,91,183]
+function setup() 
+{
+  createCanvas(400,400);
+  var sum=weights[0]+weights[1]+weights[2]+weights[3]+weights[4]
+  var average=sum/weights.length
+  console.log (average)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
